b42bdf3ac2f3b7dff2b4ab2728a695e1c2e32b62
Kuroki Almajiro <kuroki@almajiro.net>
2018-11-11 17:02:30 +0900

    Code Improvement

--- display.py
+++ display.py
@@ -138,11 +138,11 @@ if __name__ == '__main__':
             message1_length = graphics.DrawText(canvas, font, message1_position, 14, message1_color, message1)
             message2_length = graphics.DrawText(canvas, font, message2_position, 28, message2_color, message2)
 
-            if message1_scroll_speed) < message1_scroll_counter:
+            if message1_scroll_speed < message1_scroll_counter:
                 message1_position -= 1
                 message1_scroll_counter = 0
 
-            if message2_scroll_speed) < message2_scroll_counter:
+            if message2_scroll_speed < message2_scroll_counter:
                 message2_position -= 1
                 message2_scroll_counter = 0
 
